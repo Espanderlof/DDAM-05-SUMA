0503671b9d198fb2d5be00eeb9f1389d8b69b479
espanderlof <jaime717@hotmail.es>
2023-03-03 18:33:24 -0300

    fin suma

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [valor1, setValor1] = useState('');
+  const [valor2, setValor2] = useState('');
+
+  const sumar = () => {
+    const resultado = parseFloat(valor1) + parseFloat(valor2);
+    alert(`El resultado de la suma es ${resultado}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        placeholder='Ingrese el primer valor'
+        value={valor1}
+        onChangeText={(text) => setValor1(text)}
+      />
+      <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        placeholder='Ingrese el segundo valor'
+        value={valor2}
+        onChangeText={(text) => setValor2(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={sumar}>
+        <Text style={styles.buttonText}>Sumar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +36,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
   },
 });
